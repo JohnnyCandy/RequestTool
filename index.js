74b1e66929f3a06c9f6f51530a1dee13418d6a20
Johnny <4it1010024@gmail.com>
2019-04-06 22:31:49 +0800

    Update 4/6
     decrypt not finish

--- index.js
+++ index.js
@@ -1,6 +1,112 @@
-const Koa = require('koa')
-const app = new Koa()
-app.use(async (ctx, next) =>{
-ctx.body = 'HI 歡迎來到Koa的世界'
-})
-app.listen(6688)
+const Koa = require('koa'); 
+const Router = require('koa-router'); 
+const app = new Koa();  
+const router = Router(); 
+const bodyParser = require('koa-bodyparser'); 
+const md5 = require('MD5');  
+const forge = require('node-forge');
+const request = require('request');
+router.get('/', async(ctx) => {     
+    ctx.body = `     
+        <form method="POST" action="/">             
+            <label>Method</label>         
+            <input name="method" style="width:1000px"/><br/>
+            <label>URL</label>         
+            <input name="url" style="width:1000px"/><br/>  
+            <label>Token</label>         
+            <input name="token" style="width:1000px"/><br/>           
+            <button type="submit">submit</button>       
+        </form>     
+    `; 
+});
+router.post('/', async(ctx) => {     
+    let method = ctx.request.body.method + '&token=' + ctx.request.body.token;
+    let url = ctx.request.body.url;
+    let MD5 = md5(method + '&3454693@').toUpperCase();
+    let DES = encrypt(method + '&sign=' + MD5);
+    let result = '';
+    request.post(
+        url+'/api?'+DES,
+        { json: { key: 'value' } },
+        function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+                result = decrypt(body);
+                console.log(body);
+            }
+        }
+    );
+    console.log(url+'/api?'+DES);
+    ctx.body = `
+        <form method="POST" action="/">     
+            <label>Method:</label>
+            <label>${method}</label><br/>        
+            <label>MD5:</label> 
+            <label>${MD5}</label><br/>  
+            <label>DES:</label> 
+            <label>${DES}</label><br/> 
+            <label>result:</label> 
+            <label>${result}</label>
+        </form>
+    `; 
+});
+// openssl enc -des3 -in input.txt -out input.enc
+function encrypt(password) {
+    var input = password;
+   
+    // DES key and IV sizes
+    var keySize = 24;
+    var ivSize = 0;
+   
+    // get derived bytes
+    // Notes:
+    // 1. If using an alternative hash (eg: "-md sha1") pass
+    //   "forge.md.sha1.create()" as the final parameter.
+    // 2. If using "-nosalt", set salt to null.
+    var salt = forge.random.getBytesSync(8);
+    // var md = forge.md.sha1.create(); // "-md sha1"
+    var derivedBytes = forge.pbe.opensslDeriveBytes(
+      password, salt, keySize + ivSize/*, md*/);
+    var buffer = forge.util.createBuffer(derivedBytes);
+    var key = '3454693@';
+    var iv = buffer.getBytes(ivSize);
+   
+    var cipher = forge.cipher.createCipher('DES-ECB', key);
+    cipher.start({iv: iv});
+    cipher.update(forge.util.createBuffer(input, 'binary'));
+    cipher.finish();
+    var encrypted = cipher.output;
+    return encrypted.toHex();
+}
+// openssl enc -d -des3 -in input.enc -out input.dec.txt
+function decrypt(password) {
+    var input = password;
+   
+    // parse salt from input
+    input = forge.util.createBuffer(input, 'binary');
+    // read 8-byte salt
+    var salt = input.getBytes(8);
+   
+    // Note: if using "-nosalt", skip above parsing and use
+    // var salt = null;
+   
+    // DES key and IV sizes
+    var keySize = 24;
+    var ivSize = 0;
+   
+    var derivedBytes = forge.pbe.opensslDeriveBytes(
+      password, salt, keySize + ivSize);
+    var buffer = forge.util.createBuffer(derivedBytes);
+    var key = '3454693@';
+    var iv = buffer.getBytes(ivSize);
+   
+    var decipher = forge.cipher.createDecipher('DES-ECB', key);
+    decipher.start({iv: iv});
+    decipher.update(input);
+    decipher.finish(); // check 'result' for true/false
+    var decrypted = decipher.output;
+    console.log(decrypted);
+    return decrypted.toHex();
+  }
+app.use(bodyParser());
+app.use(router.routes());  
+app.listen(3001);
